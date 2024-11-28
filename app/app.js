3b5bb5565ebda42816f6f7f2615c125c7d29979d
basicplayer01 <103487181+basicplayer01@users.noreply.github.com>
2024-11-28 22:14:22 +0900

    Refractoring

---
+++ app/app.js
@@ -0,0 +1,20 @@
+// const http = require("http");
+// const app = http.createServer((req, res) => {
+//     res.end(`This is ${req.url} Page`);
+// });
+// const PORT = 3000;
+
+// app.listen(PORT, () => {
+//     console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+const express = require("express");
+const app = express();
+
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+const home = require("./src/routes/home");
+app.use("/", home); // use() : Method to Register MiddleWare
+
+module.exports = app;
