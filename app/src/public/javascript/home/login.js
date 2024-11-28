7eaeb2d3d2eaeb99656e5afedca17048dda95128
basicplayer01 <103487181+basicplayer01@users.noreply.github.com>
2024-11-28 22:54:04 +0900

    Using DOM to store HTML object

--- app/src/public/javascript/home/login.js
+++ app/src/public/javascript/home/login.js
@@ -1,4 +1,16 @@
 "use strict";
 
-console.log("Hello");
-console.log("Bye");
+const id = document.querySelector("#id"),
+    pw = document.querySelector("#pw"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id : id.value,
+        pw : pw.value,
+    }
+
+    console.log(req);
+}
