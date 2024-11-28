92eb05623314c292a56f6d6c7f25d1ffb6e8bb01
basicplayer01 <103487181+basicplayer01@users.noreply.github.com>
2024-11-28 23:03:49 +0900

    Create fetch to request login

--- app/src/public/javascript/home/login.js
+++ app/src/public/javascript/home/login.js
@@ -11,6 +11,13 @@ function login() {
         id : id.value,
         pw : pw.value,
     }
-
     console.log(req);
+    console.log(JSON.stringify(req));
+    fetch("/login", {
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json",
+        },
+        body : JSON.stringify(req),
+    });
 }
