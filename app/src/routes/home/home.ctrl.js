83476525f4be2844f35e7d3b779afcc47495892c
basicplayer01 <103487181+basicplayer01@users.noreply.github.com>
2024-11-29 14:32:03 +0900

    write login with BackEnd

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,13 +1,37 @@
 "use strict";
 
-const rootController = (req, res) => {
-    res.render("home/index");
-};
-const loginController = (req, res) => {
-    res.render("home/login");
+const users = {
+    id : ["js", "nodejs", "express"],
+    pw : ["1234", "1234", "123456"],
 };
 
+const output = {
+    rootController : (req, res) => {
+        res.render("home/index");
+    },
+    loginController : (req, res) => {
+        res.render("home/login");
+    },
+}
+const process = {
+    login : (req, res) => {
+        const id = req.body.id;
+        const pw = req.body.pw;
+
+        if(users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if(users.pw[idx] === pw) {
+                return res.json({ success : true });
+            }
+        }
+
+        return res.json({
+            success : false,
+            msg : "failed login",
+        });
+    }
+}
 module.exports = {
-    rootController,
-    loginController,
+    output,
+    process,
 };
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -4,8 +4,9 @@ const express = require("express");
 const router = express.Router();
 const controller = require("./home.ctrl");
 
-router.get('/', controller.rootController);
+router.get('/', controller.output.rootController);
 
-router.get('/login', controller.loginController);
+router.get('/login', controller.output.loginController);
+router.post('/login', controller.process.login);
 
 module.exports = router;
