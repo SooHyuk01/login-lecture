d39c2df33f607d47830d3e09d349dbbc88973a77
basicplayer01 <103487181+basicplayer01@users.noreply.github.com>
2024-12-04 22:21:49 +0900

    nodemon 적용

--- app/src/public/javascript/home/login.js
+++ app/src/public/javascript/home/login.js
@@ -1,3 +1,11 @@
 "user strict";
 
 console.log("Hello");
+console.log("Bye");
+console.log("Hello");
+console.log("Bye");
+console.log("Hello");
+console.log("Bye");
+console.log("Hello");
+console.log("Bye");
+console.log("Error");
