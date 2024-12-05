541664b740741adb209a9169747a92a3c32094bb
basicplayer01 <103487181+basicplayer01@users.noreply.github.com>
2024-12-05 19:33:16 +0900

    백엔드에서 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -1,12 +1,16 @@
 "use strict";
 
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 const home = require("./src/routes/home/index");
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", home);
 app.use(express.static(`${__dirname}/src/public`));
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,13 +1,41 @@
 "use strict";
 
-const hello = (req, res) => {
-    res.render("home/index");
+const users = {
+    id : ["김", "나", "박", "이"],
+    pw : ["1234", "1234", "1234!", "12 34"],
 };
-const login = (req, res) => {
-    res.render("home/login");
+
+const output = {
+    hello : (req, res) => {
+        res.render("home/index");
+    },
+    login : (req, res) => {
+        res.render("home/login");
+    },
+};
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+        pw = req.body.pw;
+
+        if(users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if(users.pw[idx] === pw) {
+                return res.json({
+                    success : true,
+                });
+            }
+        }
+
+        return res.json({
+            success : false,
+            msg : "failed login",
+        });
+    }
 };
 
 module.exports = {
-    hello, 
-    login,
+    output,
+    process,
 };
